3052dbe18949612cd5d4da1d6332094509110555
jose.nunez <jose.nunez@share-now.com>
2022-05-20 07:01:13 +0200

    add more requestFunctions

--- src/test/kotlin/utils/RequestMethods.kt
+++ src/test/kotlin/utils/RequestMethods.kt
@@ -1,9 +1,11 @@
 package utils
 
+import io.restassured.RestAssured
 import io.restassured.module.kotlin.extensions.Extract
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
+import io.restassured.parsing.Parser
 import io.restassured.response.Response
 import io.restassured.specification.RequestSpecification
 /**
@@ -17,6 +19,32 @@ class RequestFunctions {
             } When {
                 log().all()
                 post(requestPath)
+            } Then {
+                log().body()
+            } Extract {
+                response()
+            }
+        }
+
+        fun getRequest(requestSpecification: RequestSpecification, requestPath: String): Response {
+            return Given {
+                spec(requestSpecification)
+            } When {
+                get(requestPath)
+            } Then {
+                log().body()
+            } Extract {
+                response()
+            }
+        }
+
+        fun putRequest(requestSpecification: RequestSpecification, requestPath: String): Response {
+            RestAssured.defaultParser = Parser.JSON
+            return Given {
+                spec(requestSpecification)
+            } When {
+                log().all()
+                put(requestPath)
             } Then {
                 log().body()
             } Extract {
